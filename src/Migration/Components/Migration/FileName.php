df55f84ebf521dddc2e819d840817c1ed5ba6ee7
getintouch@icomefromthenet.com <=>
2012-03-01 21:33:26 +1100

    wrote unit test for migration file name parser

--- src/Migration/Components/Migration/FileName.php
+++ src/Migration/Components/Migration/FileName.php
@@ -9,6 +9,8 @@ class FileName
     const FORMATT = 'd_m_Y_H_i_s';
 
     /**
+     * Gets a time stamp from a file name created using
+     * the generate function
      *
      * @param string $filepath
      * @return $timestamp
@@ -17,7 +19,8 @@ class FileName
     {
         $stamp = \str_ireplace(self::SUFFIX, '', \basename(\rtrim($filepath,'.php')));
 
-        $parsed_date= \DateTime::createFromFormat(
+
+        $parsed_date = \DateTime::createFromFormat(
                 self::FORMATT, $stamp
         );
 
@@ -30,11 +33,18 @@ class FileName
 
     public function generate()
     {
-        $dte = new DateTime();
+        $dte = new \DateTime();
 
-        $stamp = $dte->format('U');
+        $stamp = array(
+            $dte->format('d'),
+            $dte->format('m'),
+            $dte->format('Y'),
+            $dte->format('H'),
+            $dte->format('i'),
+            $dte->format('s'),
+        );
 
-        return $stamp . $suffix;
+        return implode('_',$stamp) . self::SUFFIX;
 
     }
 
---
+++ tests/MigrationFilenameTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Migration\Components\Migration;
+
+require_once __DIR__ .'/base/AbstractProject.php';
+
+class MigrationFilenameTest extends AbstractProject
+{
+
+
+    public function testGenerate()
+    {
+        $file = new \Migration\Components\Migration\FileName();
+        $stamp = $file->generate();
+
+        $this->assertStringMatchesFormat('%d_%d_%d_%d_%d_%d_Migration',$stamp);
+
+    }
+
+    public function testParseGoodFormat()
+    {
+        $file = new \Migration\Components\Migration\FileName();
+        $stamp = $file->generate();
+        $file_name = $file->parse($stamp);
+
+        $this->assertStringMatchesFormat('%d',$file_name);
+    }
+
+
+
+}
+/* End of File */
